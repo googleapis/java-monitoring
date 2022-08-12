4c5f08615d25d9bf8372691e2d4aaa04d231703f
Daniel Sanche <d.sanche14@gmail.com>
2022-08-12 16:00:35 -0700

    chore: added extra variables owlbot kokoro configs

--- owlbot.py
+++ owlbot.py
@@ -43,3 +43,25 @@ s.remove_staging_dirs()
 java.common_templates(excludes=[
     '.kokoro/dependencies.sh',
 ])
+
+# --------------------------------------------------------------------------
+# Modify test configs
+# --------------------------------------------------------------------------
+
+# add shared environment variables to test configs
+s.move(
+    ".kokoro/common_env_vars.cfg",
+    ".kokoro/common.cfg",
+    merge=lambda src, dst, _, : f"{dst}\n{src}",
+)
+tracked_subdirs = ["continuous", "presubmit", "release", "nightly"]
+for subdir in tracked_subdirs:
+    for path, subdirs, files in os.walk(f".kokoro/{subdir}"):
+        for name in files:
+            if name == "common.cfg":
+                file_path = os.path.join(path, name)
+                s.move(
+                    ".kokoro/common_env_vars.cfg",
+                    file_path,
+                    merge=lambda src, dst, _, : f"{dst}\n{src}",
+                )
